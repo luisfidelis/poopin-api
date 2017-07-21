685b756fd925e430a7ca22610270ff81f0d535ac
Phil <luizsale10@gmail.com>
2017-07-20 23:49:15 -0300

    user login

--- scripts/server.js
+++ scripts/server.js
@@ -29,7 +29,7 @@ server.register({
   		host: 'localhost' 
 	});
 
-    server.route( {
+    server.route([{
         method: 'POST',
         path: '/user/save',
         handler: function handler(request, reply) {
@@ -42,13 +42,21 @@ server.register({
             var userBirthDate = params.birthDate;
             var userMail     = params.email;
             var userPassword  = params.password; 
+            var response = {
+            	"error" : false,
+            	"message": "ok",
+            	"data":[]
+            }
             if(userId){
 	            db.collection('users').findOne({_id: new ObjectID(request.params.id) }, function (err, result) {
 	                if (err) {
-	                    console.log('error');
+	                    response.error = true;
+	                    response.message = "fail";
+	                    return reply.response(response);
 	                }
 	                if(result){
-	                	// update user info
+	                	response.message("Em construção");
+	                	return reply.response(response);
 	                }
 	            });
 	        }else{
@@ -58,10 +66,67 @@ server.register({
 	            	birthDate : userBirthDate,
 	            	email : userMail,
 	            	password : userPassword 
+	            }, function(err, result){
+	            	if(err){
+	            		response.error = true;
+	                    response.message = "fail";
+	                    return reply.response(response);
+	            	}
+
+	            	db.collection('users').findOne({email: userMail}, function (err, result) {
+		                if (err) {
+		                    response.error = true;
+		                    response.message = "fail";
+		                    return reply.response(response);
+		                }
+		                if(result){
+		                	var user = result;
+		                	response.data.push({
+		                		"id" : user._id
+		                	});
+		                	return reply.response(response);
+		                }
+	           		});
 	            });
 	        }    
         }
-    });
+    },{
+    	method: 'POST',
+        path: '/user/login',
+        handler: function handler(request, reply) {
+            const db = request.mongo.db;
+            var params = request.payload;
+            var userMail      = params.email;
+            var userPassword  = params.password; 
+            var response = {
+            	"error" : false,
+            	"message": "ok",
+            	"data":[]
+            }
+            db.collection('users').findOne({email: userMail}, function (err, result) {
+                if (err) {
+                    response.error = true;
+                    response.message = "fail";
+                    return reply.response(response);
+                }
+                if(result && result.password == userPassword){
+                	var user = result;
+                    response.data.push({
+                		"email" : user.email,
+                		"name" :  user.name,
+                		"nickname" : user.nickname,
+                		"birthDate" : user.birthDate
+                	});
+                	return reply.response(response);
+                }else{
+                	response.error = true;
+                	response.message = "Incorrect email or password";
+                	return reply.response(response);
+                }
+            });
+	          
+        }
+    }]);
 
     server.start(function() {
         console.log(`Server started at ${server.info.uri}`);
