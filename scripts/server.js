57bc6f48af0329527f3650c548844ea5c3c5ad55
Phil <luizsale10@gmail.com>
2017-07-29 20:43:56 -0300

    clean handlers on server defition

--- scripts/server.js
+++ scripts/server.js
@@ -1,6 +1,10 @@
 'use strict';
 
+module = {};
+
 const Hapi = require('hapi');
+
+
 
 const server = new Hapi.Server();
 
@@ -12,8 +16,10 @@ const dbOpts = {
     decorate: true
 };
 
-//
 
+// --- Services
+
+var UserService = require("./service/user.js");
 
 
 server.register({
@@ -27,106 +33,17 @@ server.register({
 
     server.connection({
         port : process.env.PORT ||3000 ,
-  		host: '0.0.0.0' 
+  		host: '0.0.0.0'
 	});
 
     server.route([{
         method: 'POST',
         path: '/user/save',
-        handler: function handler(request, reply) {
-            const db = request.mongo.db;
-            const ObjectID = request.mongo.ObjectID;
-            var params = request.payload;
-            var userId    = params.id;
-            var userName  = params.name;
-            var userNickname  = params.nickname;
-            var userBirthDate = params.birthDate;
-            var userMail     = params.email;
-            var userPassword  = params.password; 
-            var response = {
-            	"error" : false,
-            	"message": "ok",
-            	"data":[]
-            }
-            if(userId){
-	            db.collection('users').findOne({_id: new ObjectID(request.params.id) }, function (err, result) {
-	                if (err) {
-	                    response.error = true;
-	                    response.message = "fail";
-	                    return reply.response(response);
-	                }
-	                if(result){
-	                	response.message("Em construção");
-	                	return reply.response(response);
-	                }
-	            });
-	        }else{
-	        	db.collection('users').insertOne({
-	            	name : userName,
-	            	nickname : userNickname,
-	            	birthDate : userBirthDate,
-	            	email : userMail,
-	            	password : userPassword 
-	            }, function(err, result){
-	            	if(err){
-	            		response.error = true;
-	                    response.message = "fail";
-	                    return reply.response(response);
-	            	}
-
-	            	db.collection('users').findOne({email: userMail}, function (err, result) {
-		                if (err) {
-		                    response.error = true;
-		                    response.message = "fail";
-		                    return reply.response(response);
-		                }
-		                if(result){
-		                	var user = result;
-		                	response.data.push({
-		                		"id" : user._id
-		                	});
-		                	return reply.response(response);
-		                }
-	           		});
-	            });
-	        }    
-        }
+        handler: UserService.save
     },{
     	method: 'POST',
         path: '/user/login',
-        handler: function handler(request, reply) {
-            const db = request.mongo.db;
-            var params = request.payload;
-            var userMail      = params.email;
-            var userPassword  = params.password; 
-            var response = {
-            	"error" : false,
-            	"message": "ok",
-            	"data":[]
-            }
-            db.collection('users').findOne({email: userMail}, function (err, result) {
-                if (err) {
-                    response.error = true;
-                    response.message = "fail";
-                    return reply.response(response);
-                }
-                if(result && result.password == userPassword){
-                	var user = result;
-                    response.data.push({
-                		"email" : user.email,
-                		"name" :  user.name,
-                		"nickname" : user.nickname,
-                		"birthDate" : user.birthDate
-                	});
-                	return reply.response(response);
-                }else{
-                	response.error = true;
-                	response.message = "Incorrect email or password";
-                	return reply.response(response);
-                }
-            });
-	          
-        }
+        handler: UserService.login
     }]);
 
     server.start(function() {
