5026a886087137fcbc6b8814346f27bf98fe21e7
Luis Philipe Fidelis <luizsale10@gmail.com>
2017-08-15 17:55:50 -0300

    avaliation

--- scripts/src/schema/avaliation.js
+++ scripts/src/schema/avaliation.js
@@ -3,8 +3,8 @@ var Schema   = mongoose.Schema;
 
 module.exports = new Schema({
 	_id         : {type: Schema.ObjectId},
-	userId      : {type: Schema.ObjectId, ref: 'users'},
-	toiletId    : {type: Schema.ObjectId, ref: 'toilets'}, 
+	userId      : {type: Schema.ObjectId},
+	toiletId    : {type: Schema.ObjectId}, 
     stars       : Number,
     observation : String
 });
