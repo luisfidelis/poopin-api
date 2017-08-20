c28aaf4b56fd7ad9e93a8e41be813ad54da8589c
Luis Philipe Fidelis <luizsale10@gmail.com>
2017-08-20 20:48:00 -0300

    starting jwt implementation

--- scripts/api/server.js
+++ scripts/api/server.js
@@ -20,7 +20,15 @@ var server   = new Hapi.Server();
 server.connection(connection);
 
 // --- Services
-
+server.register(require('hapi-auth-jwt'), (err) => {
+    if (err) throw err;
+    server.auth.strategy('jwt', 'jwt', {
+        key: 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJPbmxpbmUgSldUIEJ1aWxkZXIiLCJpYXQiOjE1MDMyNzI0OTUsImV4cCI6MTUzNDgwODQ5NSwiYXVkIjoid3d3LmV4YW1wbGUuY29tIiwic3ViIjoianJvY2tldEBleGFtcGxlLmNvbSIsIkdpdmVuTmFtZSI6IkpvaG5ueSIsIlN1cm5hbWUiOiJSb2NrZXQiLCJFbWFpbCI6Impyb2NrZXRAZXhhbXBsZS5jb20iLCJSb2xlIjpbIk1hbmFnZXIiLCJQcm9qZWN0IEFkbWluaXN0cmF0b3IiXX0.S35wmli5HaClppoB0-2cqe36B-9dMnCkaGHfaVQQNQs',
+        verifyOptions: { 
+            algorithms: ['HS256'] 
+        }
+    });
+});
 server.register({
     register: require('hapi-router'),
     options: {
