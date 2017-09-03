fadecc478f4c460cf4d4067eade11027af8c493c
Luiz Baldi <baldilp@gmail.com>
2017-09-02 23:55:17 -0300

    improved new user email and added some es6 syntax to user service :)

--- scripts/src/service/user.js
+++ scripts/src/service/user.js
@@ -1,159 +1,241 @@
 'use strict';
 
-var bcrypt 	    = require('bcrypt');
+var bcrypt = require('bcrypt');
 var SMTPService = require('./smtp.js');
-var User        = require('../model/user.js').User;
-var ObjectId    = require('mongoose').Types.ObjectId;
-var Utilities   = require('../util/util.js');
+var User = require('../model/user.js').User;
+var ObjectId = require('mongoose').Types.ObjectId;
+var Utilities = require('../util/util.js');
 
 const saltRounds = 10;
 
 function save(userRequest) {
 
-	var userId   = userRequest._id;
+	var userId = userRequest._id;
 	var userMail = userRequest.email;
 
 	var response = {
-		"error" : false,
+		"error": false,
 		"message": "ok",
-		"data":[]
-	}
+		"data": []
+	};
 
-	if(userId){
-		return new Promise(function(resolve,reject){
-			User.findOne({_id : new ObjectId(userId)},function(err, user){
+	if (userId) {
+		return new Promise((resolve, reject) => {
+			User.findOne({ _id: new ObjectId(userId) }, (err, user) => {
 				if (err) {
-	            	response.error = true;
-	            	response.message = "Falha ao salvar usuário";
-	            	resolve(response);
-	        	}
-	        	if(user){
-	        		user.name 	   = userRequest.name;
-	        		user.nickname  = userRequest.nickname;
-	        		user.birthDate = userRequest.birthDate;
-	        		User.update(user,function(err, raw){
-	        			if(err){
-	        				response.error = true;
-	            			response.message = "Falha ao atualizar usuário";
-	            			resolve(response);
-	        			}
-	        			response.message = 'Cagão editado com sucesso';
-	        			resolve(response);
-	        		});	        		
-	        	}
-			});
-				
-		});
-	}else{
-		return new Promise(function(resolve,reject){
-			User.findByEmail(userMail, function(err, result){
-	            if (err) {
-	                response.error = true;
-	                response.message = "Falha ao salvar usuário";
-	                resolve(response);
-	            }
-	            if(result){
-	            	response.error = true;
-	        		response.message = "E-mail já cadastrado";
-	        		resolve(response);
-	            }else{
-	            	bcrypt.hash(userRequest.password,saltRounds,function(err, hash){
-						if(err){
+					response.error = true;
+					response.message = "Falha ao salvar usuário";
+					resolve(response);
+				}
+
+				if (user) {
+					user.name = userRequest.name;
+					user.nickname = userRequest.nickname;
+					user.birthDate = userRequest.birthDate;
+					User.update(user, (err, raw) => {
+						if (err) {
 							response.error = true;
-					        response.message = "Falha ao salvar usuário";
-					        resolve(response);
-						}else{
-							userRequest._id      = new ObjectId();
+							response.message = "Falha ao atualizar usuário";
+							resolve(response);
+						}
+						response.message = 'Cagão editado com sucesso';
+						resolve(response);
+					});
+				}
+			});
+		});
+	} else {
+		return new Promise((resolve, reject) => {
+			User.findByEmail(userMail, (err, result) => {
+				if (err) {
+					response.error = true;
+					response.message = "Falha ao salvar usuário";
+					resolve(response);
+				}
+				if (result) {
+					response.error = true;
+					response.message = "E-mail já cadastrado";
+					resolve(response);
+				} else {
+					bcrypt.hash(userRequest.password, saltRounds, (err, hash) => {
+						if (err) {
+							response.error = true;
+							response.message = "Falha ao salvar usuário";
+							resolve(response);
+						} else {
+							userRequest._id = new ObjectId();
 							userRequest.password = hash;
-							var user  = new User(userRequest);
-							user.save(function(err, raw){
-	        					if(err){
-						    		response.error = true;
-						            response.message = "Falha ao salvar usuário";
-						            resolve(response);
-						    	}
-						    	if(raw){
-					            	// setup email data with unicode symbols
-					            	var user = raw;			
-						           	var message = 'Parabéns por se tornar um novo cagão na plataforma Poopin, '+user.name;
-						           	if(user.nickname){
-						           		message +=', ou melhor, '+user.nickname;
-						           	}
-						           	message += '.';
+							var user = new User(userRequest);
+							user.save((err, raw) => {
+								if (err) {
+									response.error = true;
+									response.message = "Falha ao salvar usuário";
+									resolve(response);
+								}
+
+								if (raw) {
+									// setup email data with unicode symbols
+									var user = raw;
+									var message = `Parabéns por se tornar um novo cagão na plataforma Poopin, ${user.name}`;
+									if (user.nickname) {
+										message += `, ou melhor, ${user.nickname}.`;
+									} else {
+										message += '.';
+									}
 
 									var mailOptions = {
-									    from    : '"L3Projects👻" <l3projectsweb@gmail.com>', // sender address
-									    to 		: user.email, // list of receivers
-									    subject : 'Novo cagão', // Subject line
-									    text    : message, // plain text body
-									    html    : '<h1>'+message+'</h1>' // html body
+										from: '"L3Projects👻" <l3projectsweb@gmail.com>', // sender address
+										to: user.email, // list of receivers
+										subject: 'Novo cagão - Poopin', // Subject line
+										text: message, // plain text body
+										html: _buildMailStructure(message) // html body
 									};
 
 									SMTPService.sendMail(mailOptions);
-	
-					            	response.data.push({
-					            		"id" : user._id.toString()
-					            	});
-					            	resolve(response);
-					            }
-	        				});
+
+									response.data.push({
+										"id": user._id.toString()
+									});
+									resolve(response);
+								}
+							});
 						}
 					});
-	            }
-	   		});
+				}
+			});
 		});
 	}
 };
 
-function login(options){
+function login(options) {
 
-    var userMail     = options.email;
-    var userPassword = options.password;
+	var userMail = options.email;
+	var userPassword = options.password;
 
-    var response = {
-    	"error" : false,
-    	"message": "ok",
-    	"data":[]
-    }
+	var response = {
+		"error": false,
+		"message": "ok",
+		"data": []
+	};
 
-    return new Promise(function(resolve,reject){
-    	User.findByEmail(userMail, function (err, result) {
-		   	if (err) {
+	return new Promise((resolve, reject) => {
+		User.findByEmail(userMail, (err, result) => {
+			if (err) {
 				response.error = true;
-		        response.message = "Falhar ao realizar login";
-		        resolve(response);
-		    }
+				response.message = "Falhar ao realizar login";
+				resolve(response);
+			}
 
-		    if(result){
-		    	var user  = result;
-		    	bcrypt.compare(userPassword, user.password, function(err, result){
-		    		if(err || !result){
-		    			response.error = true;
-		    			response.message = "E-mail ou senha incorretos";
-		    			resolve(response);
-		    		}else{
-		    			response.data.push({
-		    				"id"        : user._id,
-		    				"email" 	: user.email,
-		    				"name" 		: user.name,
-		    				"nickname"  : user.nickname,
-		    				"birthDate" : user.birthDate instanceof Date ? Utilities.formattedDate(user.birthDate) : null
-		    			});
-		    			resolve(response);
-		    		}            	
-		    	});
-		    }else{
-		    	response.error = true;
-        		response.message = "E-mail ou senha incorretos";
-        		resolve(response);
-        	}
-   		});
-   	});	
+			if (result) {
+				var user = result;
+				bcrypt.compare(userPassword, user.password, (err, result) => {
+					if (err || !result) {
+						response.error = true;
+						response.message = "E-mail ou senha incorretos";
+						resolve(response);
+					} else {
+						response.data.push({
+							"id": user._id,
+							"email": user.email,
+							"name": user.name,
+							"nickname": user.nickname,
+							"birthDate": user.birthDate instanceof Date ? Utilities.formattedDate(user.birthDate) : null
+						});
+						resolve(response);
+					}
+				});
+			} else {
+				response.error = true;
+				response.message = "E-mail ou senha incorretos";
+				resolve(response);
+			}
+		});
+	});
 };
 
+const _buildMailStructure = (message) => {
+	return `
+		<style>
+			.email-body{
+				background:#fff;
+			}
+			.email-container {
+				max-width:500px;
+				margin: 0 auto;
+			}
+			.header {
+				background:#e74c3c;
+				padding:50px;
+			}
+			.header hgroup {
+				text-align:center;
+				font-family:verdana;
+			}
+			.header hgroup h1 {
+				font-size: 2em;
+				color: #fff;
+				font-weight: bold;
+				letter-spacing:.3em;
+				text-transform:uppercase;
+			}
+			.header hgroup h2 {
+				padding:20px 0 0 0;
+				font-size:.75em;
+				color:#121212;
+			}
+			.section1 {
+				padding:20px;
+				color:#666;
+				font-family: verdana;
+				background: #ecf0f1;
+				text-align: center;
+			}
+			.section1 h1 {
+				font-size:1.2em;
+				color: #121212;
+				text-align: center;
+			}
+			.section1 p {
+				line-height:1.5em;
+				padding: 20px 0;
+				font-size:.75em;
+				text-align: center;
+			}
+			.section1 img {
+				width:60%;
+				padding-top:20px;
+			}
+			.footer{
+				padding:20px;
+				text-align:center;
+				background:#7f8c8d;
+				color:#A0AAAB;
+			}
+		</style>
+		<div class="email-body">
+			<div class="email-container">
+			<div class="header">
+				<hgroup>
+				<h1>Olá, seu cagão!</h1>
+				<h2>Seu cadastro foi realizado com sucesso</h2>
+				</hgroup>      
+			</div>
+			<div class="section1">
+				<h1>${message}</h1>
+				<img src="https://i.imgur.com/vhanSZD.png">
+				<p>:)</p>
+			</div>
+			<div class="footer">
+				<p>Site do Poopin</p>
+			</div>
+			</div>
+		</div>
+	`;
+}
+
 var exports = module.exports = {
-	save  : save,
-	login : login
+	save: save,
+	login: login
 };
 
 
